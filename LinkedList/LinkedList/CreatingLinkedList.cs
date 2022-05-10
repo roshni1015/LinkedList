d069d8d9bc2e307c577f1d788a8f22a17f5f694f
roshni1015 <roshniadatrao1110@gmail.com>
2022-05-10 17:29:18 +0530

    Creating a Linked List

---
+++ LinkedList/LinkedList/CreatingLinkedList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class CreatingLinkedList<M>
+    {
+        public int count = 0;
+
+        Node<M> head;
+        public void AddToLast(M Value)
+        {
+            Node<M> temp = new Node<M>(Value);
+
+            temp.data = Value;
+            temp.next = null;
+       
+            if (head == null)
+            {
+                head = temp;
+            }
+            else
+            {
+                Node<M> temp1 = new Node<M>(Value);
+                temp1 = head;
+                while (temp1.next != null)
+                    temp1 = temp1.next;
+
+                temp1.next = temp;
+            }
+            count++;
+        }
+
+        public void display()
+        {
+            Node<M> temp1 = head;
+            while (temp1 != null)
+            {
+                Console.Write("->{0}", temp1.data);
+                temp1 = temp1.next;
+            }
+        }
+    }
+}
+   
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LinkedList;
+
 Console.WriteLine("Introducing LinkedList!");
+while (true)
+{
+    Console.WriteLine("PLease choose the option :\n1)Creating LinkedList");
+    int option = Convert.ToInt16(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            CreatingLinkedList<int> Lvalue = new();
+            Lvalue.AddToLast(56);
+            Lvalue.AddToLast(30);
+            Lvalue.AddToLast(70);
+            int length = Lvalue.count;
+            Console.WriteLine("Number of elements in the LinkedList are: {0}", length);
+            Lvalue.display();
+            Console.WriteLine("\n");
+            break;
+        default:
+            Console.WriteLine("Worng Input!");
+            break;
+    }
+}
